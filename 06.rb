3fbd0fd78954e9da77772a63ca04e2a0cf0b5da5
Vacek86 <54477501+Vacek86@users.noreply.github.com>
2019-08-26 22:03:13 +0200

    Create 06.rb

---
+++ 06.rb
@@ -0,0 +1,9 @@
+# 6) Write a program to check if one given temperatures is less than 0 and the other is greater than 100.
+
+temp = gets.to_i
+
+if temp < 0
+   "temperatures is less than 0"
+elsif temp > 100
+    "greater than 100"
+end
