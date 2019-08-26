b5a76b8475e320c6636a6d659ce03910879eade5
Vacek86 <54477501+Vacek86@users.noreply.github.com>
2019-08-26 16:50:22 +0200

    Update 14.rb

--- 14.rb
+++ 14.rb
@@ -1,5 +1,8 @@
 #14) Write a program to check whether the sequence of numbers 1, 2, 3 appears in a given array of integers somewhere.
 
 checkArray = [10, 20, 1, 2, 3, 3, 33, 15]
-
+checkArray << nil
+checkArray.unshift(nil)
 checkArray.to_s.include? "1, 2, 3"
+
+#p checkArray.to_s.match? / (, |^)1, 2, 3(,|$)/
