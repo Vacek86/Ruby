8a6b832b325a3329fc60daee6b2b36b0ef345661
Vacek86 <54477501+Vacek86@users.noreply.github.com>
2019-08-26 16:07:48 +0200

    Create 14.rb

---
+++ 14.rb
@@ -0,0 +1,5 @@
+#14) Write a program to check whether the sequence of numbers 1, 2, 3 appears in a given array of integers somewhere.
+
+checkArray = [10, 20, 1, 2, 3, 3, 33, 15]
+
+checkArray.to_s.include? "1, 2, 3"
